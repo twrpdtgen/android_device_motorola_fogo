90e4a823a82e6fd50e564c2bc97d56139e5e42f3
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-09-01 11:06:11 +0200

    fogo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: motorola Model: moto g 5G - 2024 Script version: 3.0.0

---
+++ omni_fogo.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from fogo device
+$(call inherit-product, device/motorola/fogo/device.mk)
+
+PRODUCT_DEVICE := fogo
+PRODUCT_NAME := omni_fogo
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g 5G - 2024
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="fogo_g-user 11 U1UFN34.41-71 f9f2e3 release-keys"
+
+BUILD_FINGERPRINT := motorola/fogo_g/fogo:11/U1UFN34.41-71/f9f2e3:user/release-keys
